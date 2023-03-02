2d7487e563fc2c2416cadcfcde82a2de483738ae
mthierman <mthierman@gmail.com>
2023-03-01 17:02:23 -0700

    More header cleanup

--- src/Gooey.cpp
+++ src/Gooey.cpp
@@ -1,5 +1,11 @@
 #define OEMRESOURCE
+#include "Resource.h"
+#include <WebView2.h>
 #include <dwmapi.h>
+#include <wil/com.h>
+#include <wrl.h>
+#pragma comment(lib, "dwmapi")
+// #include <winrt/Microsoft.Web.WebView2.Core.h>
 
 using namespace winrt;
 using namespace winrt::Windows;
--- src/pch.h
+++ src/pch.h
@@ -1,20 +1,4 @@
 #pragma once
-
-#include <wil/com.h>
-#include <wrl.h>
-
-#include <winrt/base.h>
-
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.System.h>
+#include <guiddef.h>
 #include <winrt/Windows.UI.Composition.h>
 #include <winrt/Windows.UI.ViewManagement.h>
-
-#include "Resource.h"
-
-#include <WebView2.h>
-
-// #include <winrt/Microsoft.Web.WebView2.Core.h>
-
-#pragma comment(lib, "dwmapi")
