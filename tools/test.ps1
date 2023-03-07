a975e65c9fcdb1277cc5669c5df5f933b4d4d1e4
mthierman <mthierman@gmail.com>
2023-03-07 12:01:11 -0700

    test.ps1

--- tools/test.ps1
+++ tools/test.ps1
@@ -1,7 +1,33 @@
-# $watcher = New-Object System.IO.FileSystemWatcher
-# $watcher.Path = Get-Location
-# $watcher.IncludeSubdirectories = $true
-# $watcher.EnableRaisingEvents = $false
-# $watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite
+$global:changed = $false
+$folder = Split-Path $PSScriptRoot
+$filter = "CMakeLists.txt"
+$notify = [IO.NotifyFilters]::LastWrite
+$change = [IO.WatcherChangeTypes]::Changed
+$timeout = 1
 
-# $watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed, 1000)
+try {
+    Write-Warning "FileSystemWatcher is monitoring $path"
+    $watcher = New-Object -TypeName IO.FileSystemWatcher -ArgumentList $folder, $filter -Property @{
+        IncludeSubdirectories = $false
+        NotifyFilter          = $notify
+        EnableRaisingEvents   = $true
+    }
+    Register-ObjectEvent $watcher "Changed" -Action { $global:changed = $true }
+
+    do {
+        while ($global:changed -eq $false) {
+            Start-Sleep -Milliseconds 1
+        }
+        $result = $watcher.WaitForChanged($change, $timeout)
+        if ($result.TimedOut) { continue }
+        # if ($watcher.WaitForChanged($change, $timeout).TimedOut) { continue }
+        Write-Host "Tada"
+        $global:changed = $false
+    } while ($true)
+}
+finally {
+    # Remove-TypeData IO.FileSystemWatcher
+    $watcher.Dispose()
+    Write-Warning "FileSystemWatcher Removed"
+    # Remove-TypeData IO.FileSystemWatcher
+}
