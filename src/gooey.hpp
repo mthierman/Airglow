aec5e7b1c80a04bbadabbdd8897958b058a0a81b
mthierman <mthierman@gmail.com>
2023-05-21 20:12:06 -0600

    Re-arrange header

--- src/gooey.hpp
+++ src/gooey.hpp
@@ -1,18 +1,25 @@
+// WEBVIEW
 static wil::com_ptr<ICoreWebView2Controller> wv_controller;
 static wil::com_ptr<ICoreWebView2> wv;
 static wil::com_ptr<ICoreWebView2Settings> wv_settings;
+void WebViewNavigate(wil::com_ptr<ICoreWebView2> wv);
+
+// WINMAIN & WNDPROC
+int APIENTRY wWinMain(HINSTANCE histance, HINSTANCE hprevinstance, PWSTR pcmdline, int ncmdshow);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
+
+// THEMING
 HBRUSH hollowBrush;
 HBRUSH darkBrush;
 HBRUSH lightBrush;
-LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
-int APIENTRY wWinMain(HINSTANCE histance, HINSTANCE hprevinstance, PWSTR pcmdline, int ncmdshow);
 int lightMode;
 int ModeCheck();
 void DarkTitle();
 void DarkMode(HWND hwnd);
 void SetMica(HWND hwnd);
 void ExtendFrame(HWND hwnd);
+
+// KEYBOARD SHORTCUTS
 void KeyTop(HWND hwnd);
 void KeyMaximize(HWND hwnd);
 void KeyFullscreen(HWND hwnd);
-void WebViewNavigate(wil::com_ptr<ICoreWebView2> wv);
