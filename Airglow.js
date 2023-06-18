06db4a2f16cc5c30c55d872c314c61939468c9d7
mthierman <mthierman@gmail.com>
2023-06-17 20:10:28 -0600

    Update js file with both scripts

--- Airglow.js
+++ Airglow.js
@@ -1,4 +1,15 @@
+// BROWSERS
 document.onreadystatechange = () => {
+    if (document.readyState === "interactive") {
+        let scheme = document.createElement("meta");
+        scheme.setAttribute("name", "color-scheme");
+        scheme.setAttribute("content", "light dark");
+        document.getElementsByTagName("head")[0].appendChild(scheme);
+        document.documentElement.style.setProperty(
+            "color-scheme",
+            "light dark"
+        );
+    }
     if (document.readyState === "complete") {
         onkeydown = (e) => {
             if (e.ctrlKey && e.key === "w") {
@@ -6,14 +17,32 @@ document.onreadystatechange = () => {
             } else {
                 window.chrome.webview.postMessage(e.key);
             }
-            let scheme = document.createElement("meta");
-            scheme.setAttribute("name", "color-scheme");
-            scheme.setAttribute("content", "light dark");
-            document.getElementsByTagName("head")[0].appendChild(scheme);
-            document.documentElement.style.setProperty(
-                "color-scheme",
-                "light dark"
-            );
+        };
+    }
+};
+
+// SETTINGS
+document.onreadystatechange = () => {
+    if (document.readyState === "interactive") {
+        let scheme = document.createElement("meta");
+        scheme.setAttribute("name", "color-scheme");
+        scheme.setAttribute("content", "light dark");
+        document.getElementsByTagName("head")[0].appendChild(scheme);
+        document.documentElement.style.setProperty(
+            "color-scheme",
+            "light dark"
+        );
+    }
+    if (document.readyState === "complete") {
+        onkeydown = (e) => {
+            if (e.key === "F3") {
+                e.preventDefault();
+            }
+            if (e.ctrlKey && e.key === "w") {
+                window.chrome.webview.postMessage("close");
+            } else {
+                window.chrome.webview.postMessage(e.key);
+            }
         };
     }
 };
