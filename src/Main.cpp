ec9dd4b21b1b3586a482b247b8c8dbe1b1e87d6d
mthierman <mthierman@gmail.com>
2023-06-17 23:51:39 -0600

    Set fullscreen and max on startup

--- src/Main.cpp
+++ src/Main.cpp
@@ -35,6 +35,10 @@ void Startup()
     LoadSettings();
     Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
     SetWindowPos(window, nullptr, dimensions[0], dimensions[1], dimensions[2], dimensions[3], 0);
+    if (maximized)
+        ShowWindow(window, SW_SHOWMAXIMIZED);
+    if (fullscreen)
+        FullscreenWindow(window);
 }
 
 void Shutdown()
