09c166e1f2cc3ed3571ea58a14618ac3c11c23d5
mthierman <mthierman@gmail.com>
2023-06-22 20:39:21 -0600

    Better path to DB

--- src/Database.cxx
+++ src/Database.cxx
@@ -12,12 +12,18 @@ std::unique_ptr<Database> Database::Create(Config* config)
 
     sqlite3* db;
 
-    path dbFilePath =
-        (ToWide(pConfig->pathData.string())) + path::preferred_separator + L"Airglow.db";
-    auto dbFilePathConvert = ToString(dbFilePath);
-    const char* dbPath = dbFilePathConvert.c_str();
+    path dbFile = pConfig->pathData.wstring() + path::preferred_separator + L"Airglow.db";
+    auto dbFileS = dbFile.string();
+    const char* dbPath = dbFileS.c_str();
 
     auto dbOpen = sqlite3_open(dbPath, &db);
+
+    if (dbOpen)
+    {
+        string err = "Database opening failed";
+        error(err);
+        return 0;
+    }
 
     return pDatabase;
 }
