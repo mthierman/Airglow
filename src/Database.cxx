446180aebd857d9205abcb8272230eec1e49a0ae
mthierman <mthierman@gmail.com>
2023-06-22 20:14:53 -0600

    Working pConfig

--- src/Database.cxx
+++ src/Database.cxx
@@ -13,7 +13,7 @@ std::unique_ptr<Database> Database::Create(Config* config)
     sqlite3* db;
 
     path dbFilePath =
-        (ToWide(config->pathData.string())) + path::preferred_separator + L"Airglow.db";
+        (ToWide(pConfig->pathData.string())) + path::preferred_separator + L"Airglow.db";
     auto dbFilePathConvert = ToString(dbFilePath);
     const char* dbPath = dbFilePathConvert.c_str();
 
