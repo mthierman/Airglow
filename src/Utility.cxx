586ee7a3f5a5ee902933611d586d72971972fd51
mthierman <mthierman@gmail.com>
2023-06-26 14:40:43 -0600

    Parse URL on command line

--- src/Utility.cxx
+++ src/Utility.cxx
@@ -3,30 +3,62 @@
 namespace Utility
 {
 
-std::pair<std::wstring, std::wstring> CommandLine()
+std::pair<wstring, wstring> CommandLine()
 {
-    std::pair<std::wstring, std::wstring> pair;
-    int number;
+    std::pair<wstring, wstring> commands;
 
     auto cmd = GetCommandLineW();
+    int number;
+
     auto args = CommandLineToArgvW(cmd, &number);
 
     if (number == 2)
     {
-        pair.first = args[1];
-        pair.second = args[1];
+        commands.first = args[1];
+        commands.second = args[1];
     }
 
     if (number == 3)
     {
-        pair.first = args[1];
-        pair.second = args[2];
+        commands.first = args[1];
+        commands.second = args[2];
     }
 
     LocalFree(args);
 
-    return pair;
+    if (!commands.first.starts_with(L"http") || !commands.first.starts_with(L"https"))
+        commands.first = L"https://" + commands.first;
+
+    if (!commands.second.starts_with(L"http") || !commands.second.starts_with(L"https"))
+        commands.second = L"https://" + commands.second;
+
+    return commands;
 }
+
+// std::pair<std::wstring, std::wstring> CommandLine()
+// {
+//     std::pair<std::wstring, std::wstring> pair;
+//     int number;
+
+//     auto cmd = GetCommandLineW();
+//     auto args = CommandLineToArgvW(cmd, &number);
+
+//     if (number == 2)
+//     {
+//         pair.first = args[1];
+//         pair.second = args[1];
+//     }
+
+//     if (number == 3)
+//     {
+//         pair.first = args[1];
+//         pair.second = args[2];
+//     }
+
+//     LocalFree(args);
+
+//     return pair;
+// }
 
 bool Toggle(bool b) { return b ? false : true; }
 
