43a9fbab77a8e0d603617f87d0b576149f8e6f18
mthierman <mthierman@gmail.com>
2023-07-07 19:10:14 -0600

    Project name & version in JSON

--- src/Airglow.cxx
+++ src/Airglow.cxx
@@ -2,10 +2,7 @@
 
 int __stdcall wWinMain(HINSTANCE hinstance, HINSTANCE hpinstance, PWSTR pcl, int ncs)
 {
-    MSG msg{};
-
     auto pConfig = Config::Create();
-
     if (!pConfig || !std::filesystem::exists(pConfig->pathData))
     {
         error("Data folder not found");
@@ -13,7 +10,6 @@ int __stdcall wWinMain(HINSTANCE hinstance, HINSTANCE hpinstance, PWSTR pcl, int
     }
 
     auto pDatabase = Database::Create(pConfig.get());
-
     if (!pDatabase)
     {
         error("Database creation failed");
@@ -21,19 +17,16 @@ int __stdcall wWinMain(HINSTANCE hinstance, HINSTANCE hpinstance, PWSTR pcl, int
     }
 
     auto pWindow = MainWindow::Create(hinstance, ncs, pConfig.get());
+    pWindow->Show(pWindow->m_hwnd, ncs);
 
-    auto hwnd = pWindow->m_hwnd;
-
-    pWindow->Show(hwnd, ncs);
-
-    auto pWebView = WebView::Create(hwnd, pConfig.get());
-
+    auto pWebView = WebView::Create(pWindow->m_hwnd, pConfig.get());
     if (!pWebView)
     {
         error("WebView2 creation failed");
         return 0;
     }
 
+    MSG msg{};
     while (GetMessageW(&msg, nullptr, 0, 0))
     {
         TranslateMessage(&msg);
