c4b94512f19e6cce605e23ec4a03aa6de5f7a59e
mthierman <mthierman@gmail.com>
2023-07-12 08:41:50 -0600

    Fix prettier

--- gui/src/app.tsx
+++ gui/src/app.tsx
@@ -1,46 +1,65 @@
+import {
+    makeStyles,
+    FluentProvider,
+    webLightTheme,
+    webDarkTheme,
+    Button,
+} from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+    provider: {
+        backgroundColor: "transparent",
+        height: "100%",
+    },
+});
+
 export default function App() {
     function handleForm(e: any) {
         e.preventDefault();
         const data = new FormData(e.target);
         const mainUrl = Object.fromEntries(data.entries()).mainUrl;
         const sideUrl = Object.fromEntries(data.entries()).sideUrl;
-        // console.log(main);
-        // console.log(side);
         window.chrome.webview.postMessage(`mainUrl ${mainUrl}`);
         window.chrome.webview.postMessage(`sideUrl ${sideUrl}`);
     }
 
+    const styles = useStyles();
+
     return (
-        <form
-            autoComplete="off"
-            id="settings"
-            method="post"
-            onSubmit={handleForm}>
-            <div id="url">
-                <label>
-                    Main URL
-                    <input
-                        type="url"
-                        name="mainUrl"
-                        placeholder="https://"
-                        pattern="https://.*"
-                        required
-                    />
-                </label>
+        <FluentProvider className={styles.provider} theme={webDarkTheme}>
+            <form
+                autoComplete="off"
+                id="settings"
+                method="post"
+                onSubmit={handleForm}>
+                <div id="url">
+                    <label>
+                        Main URL
+                        <input
+                            type="url"
+                            name="mainUrl"
+                            placeholder="https://"
+                            pattern="https://.*"
+                            required
+                        />
+                    </label>
 
-                <label>
-                    Side URL
-                    <input
-                        type="url"
-                        name="sideUrl"
-                        placeholder="https://"
-                        pattern="https://.*"
-                        required
-                    />
-                </label>
-            </div>
+                    <label>
+                        Side URL
+                        <input
+                            type="url"
+                            name="sideUrl"
+                            placeholder="https://"
+                            pattern="https://.*"
+                            required
+                        />
+                    </label>
+                </div>
 
-            <button type="submit">Save</button>
-        </form>
+                <Button appearance="secondary" type="submit">
+                    💾 Save
+                </Button>
+            </form>
+        </FluentProvider>
     );
 }
