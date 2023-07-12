9293da5fe579e6f349efb59c75ea84a8cfd97bc0
mthierman <mthierman@gmail.com>
2023-07-12 08:24:58 -0600

    Update node_modules

---
+++ tools/clean_node.ps1
@@ -0,0 +1,3 @@
+$repo = $PSScriptRoot | Split-Path
+if (Test-Path "$repo/gui/node_modules") { Remove-Item "$repo/gui/node_modules" -Force -Recurse }
+if (Test-Path "$repo/gui/pnpm-lock.yaml") { Remove-Item "$repo/gui/pnpm-lock.yaml" -Force -Recurse }
