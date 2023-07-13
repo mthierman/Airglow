7804ac5407ce16e6015a798e3745186e853413e7
mthierman <mthierman@gmail.com>
2023-07-13 15:01:45 -0600

    Minor script updates

--- tools/graphviz.ps1
+++ tools/graphviz.ps1
@@ -1,2 +1,3 @@
 $repo = $PSScriptRoot | Split-Path
+
 dot -Tsvg "$repo/build/CMakeFiles/graphviz/graphviz.dot" > "$repo/build/CMakeFiles/graphviz/graphviz.svg"
--- tools/notes.ps1
+++ tools/notes.ps1
@@ -1,7 +1,9 @@
 $repo = $PSScriptRoot | Split-Path
+
 if (!(Test-Path "$repo/build"))
 {
     New-Item -ItemType Directory "$repo/build"
 }
-$changes = New-Item -Path "$repo/build/notes.txt"
+
+$changes = New-Item -Path "$repo/build/release_notes.txt"
 git --no-pager log -5 --oneline --no-decorate | Add-Content -Path $changes
