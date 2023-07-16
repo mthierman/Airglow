e3f79316e1ebd38e1319724c01e73e28a929c1e3
mthierman <mthierman@gmail.com>
2023-07-15 19:18:57 -0600

    All 3 browsers again

--- src/Browser.cxx
+++ src/Browser.cxx
@@ -5,11 +5,22 @@ using namespace Utility;
 using namespace Gdiplus;
 using namespace Microsoft::WRL;
 
-wil::com_ptr<ICoreWebView2Environment> Browser::settings_environment{nullptr};
+wil::com_ptr<ICoreWebView2Environment> Browser::browser_environment{nullptr};
+
 wil::com_ptr<ICoreWebView2Controller> Browser::settings_controller{nullptr};
 wil::com_ptr<ICoreWebView2> Browser::settings_core{nullptr};
 wil::com_ptr<ICoreWebView2_19> Browser::settings_browser{nullptr};
 wil::com_ptr<ICoreWebView2Settings> Browser::settings_settings{nullptr};
+
+wil::com_ptr<ICoreWebView2Controller> Browser::main_controller{nullptr};
+wil::com_ptr<ICoreWebView2> Browser::main_core{nullptr};
+wil::com_ptr<ICoreWebView2_19> Browser::main_browser{nullptr};
+wil::com_ptr<ICoreWebView2Settings> Browser::main_settings{nullptr};
+
+wil::com_ptr<ICoreWebView2Controller> Browser::side_controller{nullptr};
+wil::com_ptr<ICoreWebView2> Browser::side_core{nullptr};
+wil::com_ptr<ICoreWebView2_19> Browser::side_browser{nullptr};
+wil::com_ptr<ICoreWebView2Settings> Browser::side_settings{nullptr};
 
 Browser::Browser(HWND hwnd) {}
 
@@ -23,7 +34,7 @@ std::unique_ptr<Browser> Browser::Create(HWND hwnd)
                 [](HRESULT hr, ICoreWebView2Environment* environment) -> HRESULT
                 {
                     if (environment)
-                        settings_environment = environment;
+                        browser_environment = environment;
 
                     return S_OK;
                 })
@@ -32,7 +43,7 @@ std::unique_ptr<Browser> Browser::Create(HWND hwnd)
         return nullptr;
     };
 
-    if (FAILED(settings_environment->CreateCoreWebView2Controller(
+    if (FAILED(browser_environment->CreateCoreWebView2Controller(
             hwnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
                       [hwnd](HRESULT hr, ICoreWebView2Controller* controller) -> HRESULT
                       {
@@ -49,6 +60,58 @@ std::unique_ptr<Browser> Browser::Create(HWND hwnd)
                           {
                               settings_controller->put_Bounds(get_rect(hwnd));
                               settings_browser->Navigate(L"https://wwww.google.com/");
+                          }
+
+                          return S_OK;
+                      })
+                      .Get())))
+    {
+        return nullptr;
+    };
+
+    if (FAILED(browser_environment->CreateCoreWebView2Controller(
+            hwnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+                      [hwnd](HRESULT hr, ICoreWebView2Controller* controller) -> HRESULT
+                      {
+                          if (controller)
+                          {
+                              main_controller = controller;
+                              main_controller->get_CoreWebView2(&main_core);
+                          }
+
+                          if (main_core)
+                              main_browser = main_core.try_query<ICoreWebView2_19>();
+
+                          if (main_browser)
+                          {
+                              main_controller->put_Bounds(get_rect(hwnd));
+                              main_browser->Navigate(L"https://wwww.google.com/");
+                          }
+
+                          return S_OK;
+                      })
+                      .Get())))
+    {
+        return nullptr;
+    };
+
+    if (FAILED(browser_environment->CreateCoreWebView2Controller(
+            hwnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+                      [hwnd](HRESULT hr, ICoreWebView2Controller* controller) -> HRESULT
+                      {
+                          if (controller)
+                          {
+                              side_controller = controller;
+                              side_controller->get_CoreWebView2(&side_core);
+                          }
+
+                          if (side_core)
+                              side_browser = side_core.try_query<ICoreWebView2_19>();
+
+                          if (side_browser)
+                          {
+                              side_controller->put_Bounds(get_rect(hwnd));
+                              side_browser->Navigate(L"https://wwww.google.com/");
                           }
 
                           return S_OK;
--- src/Browser.hxx
+++ src/Browser.hxx
@@ -12,11 +12,22 @@ class Browser
     // void SetWindowIcon();
 
   private:
-    static wil::com_ptr<ICoreWebView2Environment> settings_environment;
+    static wil::com_ptr<ICoreWebView2Environment> browser_environment;
+
     static wil::com_ptr<ICoreWebView2Controller> settings_controller;
     static wil::com_ptr<ICoreWebView2> settings_core;
     static wil::com_ptr<ICoreWebView2_19> settings_browser;
     static wil::com_ptr<ICoreWebView2Settings> settings_settings;
+
+    static wil::com_ptr<ICoreWebView2Controller> main_controller;
+    static wil::com_ptr<ICoreWebView2> main_core;
+    static wil::com_ptr<ICoreWebView2_19> main_browser;
+    static wil::com_ptr<ICoreWebView2Settings> main_settings;
+
+    static wil::com_ptr<ICoreWebView2Controller> side_controller;
+    static wil::com_ptr<ICoreWebView2> side_core;
+    static wil::com_ptr<ICoreWebView2_19> side_browser;
+    static wil::com_ptr<ICoreWebView2Settings> side_settings;
 
     Browser(HWND);
     // std::pair<wstring, wstring> CommandLine();
