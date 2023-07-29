281239d724f4e04aa18bd3d9365304953863e2f4
mthierman <mthierman@gmail.com>
2023-07-28 21:51:23 -0600

    Move DB Code to tests file for now

--- src/test/Test.cxx
+++ src/test/Test.cxx
@@ -47,3 +47,47 @@ void App::Debug()
     println(std::to_string(window.position[2]));
     println(std::to_string(window.position[3]));
 }
+
+// void App::CreateDb()
+// {
+//     auto dbFile{(paths.db).string()};
+//     auto dbPath{dbFile.c_str()};
+//     sqlite3* db{nullptr};
+//     char* errMsg{0};
+//     string sql{"CREATE TABLE CONFIG("
+//                "X INT NOT NULL,"
+//                "Y INT NOT NULL,"
+//                "WIDTH INT NOT NULL,"
+//                "HEIGHT INT NOT NULL,"
+//                "MENU INT NOT NULL,"
+//                "SPLIT INT NOT NULL,"
+//                "MAXIMIZED INT NOT NULL,"
+//                "FULLSCREEN INT NOT NULL,"
+//                "TOPMOST INT NOT NULL,"
+//                "MAIN TEXT NOT NULL,"
+//                "SIDE TEXT NOT NULL);"};
+
+//     auto dbOpen = sqlite3_open(dbPath, &db);
+//     if (dbOpen != SQLITE_OK)
+//     {
+//         dberror("Database opening failed");
+//         return;
+//     }
+
+//     if (std::filesystem::exists(paths.db))
+//     {
+//         auto debExec = sqlite3_exec(db, sql.c_str(), nullptr, 0, &errMsg);
+//         if (debExec != SQLITE_OK)
+//         {
+//             dberror(errMsg);
+//             sqlite3_free(errMsg);
+//         }
+//     }
+
+//     if (!std::filesystem::exists(paths.db))
+//         return;
+
+//     sqlite3_close(db);
+
+//     return;
+// }
