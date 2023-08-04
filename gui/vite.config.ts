9f7e4aa1ea531d04f0742f467b55238ad743354c
mthierman <mthierman@gmail.com>
2023-08-04 07:42:17 -0600

    Update vite config and add /Zc:__cplusplus

--- gui/vite.config.ts
+++ gui/vite.config.ts
@@ -2,47 +2,36 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(async ({ command, mode }) => {
+export default defineConfig(async ({ command, mode, ssrBuild }) => {
+    let options = {};
     if (command === "serve") {
-        const cert = path.resolve("../../.cert/localhost.pfx");
-        const passphrase = "localhost";
-        return {
-            root: "./src",
-            base: "./",
-            plugins: [react()],
-            server: {
-                port: 8000,
-                https: {
-                    pfx: cert,
-                    passphrase: passphrase,
-                },
-            },
-            preview: {
-                port: 8000,
-                https: {
-                    pfx: cert,
-                    passphrase: passphrase,
-                },
-            },
-        };
-    } else {
-        return {
-            root: "./src",
-            base: "./",
-            plugins: [react()],
-            build: {
-                outDir: "../dist",
-                rollupOptions: {
-                    input: {
-                        main: path.resolve(__dirname, "src/index.html"),
-                        settings: path.resolve(
-                            __dirname,
-                            "src/settings/index.html",
-                        ),
-                        bar: path.resolve(__dirname, "src/bar/index.html"),
-                    },
-                },
+        options = {
+            port: 8000,
+            https: {
+                pfx: path.resolve("../../.cert/localhost.pfx"),
+                passphrase: "localhost",
             },
         };
     }
+
+    return {
+        root: "./src",
+        base: "./",
+        plugins: [react()],
+        server: options,
+        preview: options,
+        build: {
+            outDir: "../dist",
+            rollupOptions: {
+                input: {
+                    main: path.resolve(__dirname, "src/index.html"),
+                    settings: path.resolve(
+                        __dirname,
+                        "src/settings/index.html",
+                    ),
+                    bar: path.resolve(__dirname, "src/bar/index.html"),
+                },
+            },
+        },
+    };
 });
