4400eadb7de706a9ade4edf4bd907bc2d6210473
mthierman <mthierman@gmail.com>
2023-08-09 21:08:45 -0600

    Update console to use <print>

--- src/console/Main.cxx
+++ src/console/Main.cxx
@@ -1,18 +1,6 @@
-#include <iostream>
-
-// #include "../Utility.hxx"
-
-// using namespace Utility;
-
-struct App
-{
-    // wstring name{to_wide(APP_NAME)};
-    // wstring version{to_wide(APP_NAME)};
-};
-
 int main()
 {
-    std::cout << "Hello World";
+    std::println("Hello World");
 
     return 0;
 }
