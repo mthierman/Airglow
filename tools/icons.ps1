02940709d11577d8e14dafecb5b772ceb9481f12
mthierman <mthierman@gmail.com>
2023-08-30 21:02:55 -0600

    Update icons

--- tools/icons.ps1
+++ tools/icons.ps1
@@ -1,4 +1,4 @@
 $repo = $PSScriptRoot | Split-Path
 
 magick convert -background none "$repo/data/icons/icon.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/icons/icon.ico"
-magick convert -background none "$repo/data/icons/debug_icon.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/icons/debug_icon.ico"
+magick convert -background none "$repo/data/icons/debug.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/icons/debug.ico"
