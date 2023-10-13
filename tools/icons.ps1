08bda99f5fa6619cc972290a074df4933c1dad1e
mthierman <mthierman@gmail.com>
2023-10-12 23:32:13 -0600

    Fix build

--- tools/icons.ps1
+++ tools/icons.ps1
@@ -1,4 +1,4 @@
 $repo = $PSScriptRoot | Split-Path
 
-magick convert -background none "$repo/data/icons/icon.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/icons/icon.ico"
-magick convert -background none "$repo/data/icons/debug.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/icons/debug.ico"
+magick convert -background none "$repo/data/icon.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/icon.ico"
+magick convert -background none "$repo/data/debug.svg" -define icon:auto-resize="256,64,48,32,24,16" "$repo/data/debug.ico"
