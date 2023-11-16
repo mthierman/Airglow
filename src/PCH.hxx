fdd39695cb0dc42e85facc650d79cb15bdcc7893
mthierman <mthierman@gmail.com>
2023-11-15 20:17:30 -0700

    Update CMakeLists

--- src/PCH.hxx
+++ src/PCH.hxx
@@ -10,26 +10,29 @@
 
 #pragma once
 
+#include <AirglowConfig.h>
+
 #include <Windows.h>
 #include <dwmapi.h>
 #include <gdiplus.h>
-#include <ShlObj.h>
 #include <shcore.h>
-// #include <ShObjIdl.h>
+#include <ShlObj.h>
 
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <memory>
 
-#include "winrt/Windows.Foundation.h"
-#include "winrt/Microsoft.Web.WebView2.Core.h"
-#include "winrt/Windows.UI.ViewManagement.h"
-#include "winrt/Windows.Graphics.Imaging.h"
+#include <winrt/base.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#include <winrt/Windows.Graphics.Imaging.h>
+
+#include <winrt/Microsoft.Web.WebView2.Core.h>
 
 #include <nlohmann/json.hpp>
 #include <sqlite3.h>
-#include <curl.h>
+// #include <curl.h>
 
 #include "Templates.hxx"
 #include "Definitions.hxx"
