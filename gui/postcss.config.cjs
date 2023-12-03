cc44f7a4081368fa6498e510d26361f8ac78fd43
mthierman <mthierman@gmail.com>
2023-12-02 20:35:18 -0700

    Update gui packages/configs

--- gui/postcss.config.cjs
+++ gui/postcss.config.cjs
@@ -2,8 +2,8 @@
 
 module.exports = {
     plugins: {
-        tailwindcss: {},
+        "tailwindcss": {},
         "@tailwindcss/typography": {},
-        autoprefixer: {},
+        "autoprefixer": {},
     },
 };
--- gui/prettier.config.cjs
+++ gui/prettier.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('prettier').Config} */
 
 module.exports = {
+    printWidth: 100,
     tabWidth: 4,
     bracketSameLine: true,
     quoteProps: "consistent",
