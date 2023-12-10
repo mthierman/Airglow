22d5fe2051423badc1f6e4290d608edd73dfed9a
mthierman <mthierman@gmail.com>
2023-12-10 10:50:56 -0700

    Update banners/dividers

--- Airglow/PCH.cxx
+++ Airglow/PCH.cxx
@@ -1,11 +1,9 @@
 // clang-format off
-// ╔─────────────────────────────╗
-// │                             │
-// │     ╔═╗╦╦═╗╔═╗╦  ╔═╗╦ ╦     │  Airglow - https://github.com/mthierman/Airglow
-// │     ╠═╣║╠╦╝║ ╦║  ║ ║║║║     │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
-// │     ╩ ╩╩╩╚═╚═╝╩═╝╚═╝╚╩╝     │  SPDX-License-Identifier: MIT
-// │                             │
-// ╚─────────────────────────────╝
+// ╔─────────────────────╗
+// │ ╔═╗╦╦═╗╔═╗╦  ╔═╗╦ ╦ │  Airglow - https://github.com/mthierman/Airglow
+// │ ╠═╣║╠╦╝║ ╦║  ║ ║║║║ │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │ ╩ ╩╩╩╚═╚═╝╩═╝╚═╝╚╩╝ │  SPDX-License-Identifier: MIT
+// ╚─────────────────────╝
 // clang-format on
 
 #include "PCH.hxx"
