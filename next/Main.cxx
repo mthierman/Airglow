b2d4061b69d5c5ec53d3fecb9792a3d8729f2108
mthierman <mthierman@gmail.com>
2023-12-10 17:25:19 -0700

    Add main for next

---
+++ next/Main.cxx
@@ -0,0 +1,32 @@
+// clang-format off
+// ╔─────────────────────╗
+// │ ╔═╗╦╦═╗╔═╗╦  ╔═╗╦ ╦ │  Airglow - https://github.com/mthierman/Airglow
+// │ ╠═╣║╠╦╝║ ╦║  ║ ║║║║ │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │ ╩ ╩╩╩╚═╚═╝╩═╝╚═╝╚╩╝ │  SPDX-License-Identifier: MIT
+// ╚─────────────────────╝
+// clang-format on
+
+#include <gui/app.hxx>
+
+//==============================================================================
+auto WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
+    -> int
+{
+    auto app{std::make_unique<glow::gui::App>("Next")};
+
+    ::MSG msg{};
+    int r{};
+
+    while ((r = ::GetMessage(&msg, nullptr, 0, 0)) != 0)
+    {
+        if (r == -1) return 0;
+
+        else
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+        }
+    }
+
+    return 0;
+}
