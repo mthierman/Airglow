ba0eff7d5982f360c4bad68603418dde9a3bdefd
mthierman <mthierman@gmail.com>
2023-12-11 14:11:57 -0700

    Get icon working

--- next/cmake/icon.cmake
+++ next/cmake/icon.cmake
@@ -1,31 +1,33 @@
 file(
     TO_CMAKE_PATH
-    ${PROJECT_BINARY_DIR}/data/Glow.rc
-    GLOW_DATA_RC
+    ${CMAKE_CURRENT_BINARY_DIR}/data/airglow.rc
+    AIRGLOW_RC
+)
+
+file(
+    TO_CMAKE_PATH
+    ${CMAKE_CURRENT_BINARY_DIR}/data/icon.ico
+    AIRGLOW_ICON
 )
 
 file(
     GENERATE
-    OUTPUT ${GLOW_DATA_RC}
-    CONTENT "1 ICON \"Glow.ico\""
+    OUTPUT ${AIRGLOW_RC}
+    CONTENT "1 ICON \"icon.ico\""
 )
 
-# set(GLOW_ICON_SOURCE
-#     ${PROJECT_SOURCE_DIR}/data/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>.ico
-# )
+set(GLOW_ICON_SOURCE
+    ${PROJECT_SOURCE_DIR}/next/data/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>.ico
+)
 
-# set(GLOW_ICON
-#     ${PROJECT_BINARY_DIR}/data/Glow.ico
-# )
+add_custom_command(
+    OUTPUT ${AIRGLOW_ICON}
+    COMMAND ${CMAKE_COMMAND} -E copy ${GLOW_ICON_SOURCE} ${AIRGLOW_ICON}
+    COMMENT "Copying icon..."
+)
 
-# add_custom_command(
-#     OUTPUT ${GLOW_ICON}
-#     COMMAND ${CMAKE_COMMAND} -E copy ${GLOW_ICON_SOURCE} ${GLOW_ICON}
-#     COMMENT "Copying icon..."
-# )
-
-# add_custom_target(
-#     MakeIcon
-#     ALL
-#     DEPENDS ${GLOW_ICON}
-# )
+add_custom_target(
+    CopyIcon
+    ALL
+    DEPENDS ${AIRGLOW_ICON}
+)
