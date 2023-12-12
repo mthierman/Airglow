5f95a4b2a625dd2759d90bcf44848830f6e4147e
mthierman <mthierman@gmail.com>
2023-12-11 19:27:34 -0700

    Fix webviews

--- next/src/settings.hxx
+++ next/src/settings.hxx
@@ -17,29 +17,29 @@ namespace airglow
 {
 
 //==============================================================================
-struct Settings
-{
-    std::string name{"test"};
-    std::string version{"test"};
-    int width{0};
-    int height{0};
-};
+// struct Settings
+// {
+//     std::string name{"test"};
+//     std::string version{"test"};
+//     int width{0};
+//     int height{0};
+// };
 
 //==============================================================================
-inline void to_json(nlohmann::json& j, const Settings& s)
-{
-    j = nlohmann::json{
-        {"name", s.name}, {"version", s.version}, {"width", s.width}, {"height", s.height}};
-}
+// inline void to_json(nlohmann::json& j, const Settings& s)
+// {
+//     j = nlohmann::json{
+//         {"name", s.name}, {"version", s.version}, {"width", s.width}, {"height", s.height}};
+// }
 
 //==============================================================================
-inline void from_json(const nlohmann::json& j, Settings& s)
-{
-    j.at("name").get_to(s.name);
-    j.at("version").get_to(s.version);
-    j.at("width").get_to(s.width);
-    j.at("height").get_to(s.height);
-}
+// inline void from_json(const nlohmann::json& j, Settings& s)
+// {
+//     j.at("name").get_to(s.name);
+//     j.at("version").get_to(s.version);
+//     j.at("width").get_to(s.width);
+//     j.at("height").get_to(s.height);
+// }
 
 //==============================================================================
 } // namespace airglow
