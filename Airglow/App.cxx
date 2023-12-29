2123b2dd70fb9a24dc1804ff2a50cb04296fa3fe
mthierman <mthierman@gmail.com>
2023-12-28 21:07:05 -0700

    Fix border color

--- Airglow/App.cxx
+++ Airglow/App.cxx
@@ -16,8 +16,8 @@ auto App::run() -> void
     App app;
     set_title(app.m_hwnd.get(), "Airglow");
     set_caption_color(app.m_hwnd.get(), false);
-    set_border_color(app.m_hwnd.get(), false);
-    set_system_backdrop(app.m_hwnd.get(), DWM_SYSTEMBACKDROP_TYPE::DWMSBT_TABBEDWINDOW);
+    set_border_color(app.m_hwnd.get(), true);
+    set_system_backdrop(app.m_hwnd.get(), DWM_SYSTEMBACKDROP_TYPE::DWMSBT_MAINWINDOW);
     set_theme(app.m_hwnd.get());
     message_loop();
 }
