b42e48e0054df930c8427390e87f053e51e3cbfe
mthierman <mthierman@gmail.com>
2024-01-02 21:57:15 -0700

    Cleanup main

--- Airglow/airglow/main.cxx
+++ Airglow/airglow/main.cxx
@@ -30,30 +30,19 @@ auto run() -> int
                             "--allow-file-access-from-files");
 
     airglow::App app;
-    app(false);
-    app.dwm_caption_color(false);
-    app.dwm_dark_mode(true);
-    app.dwm_system_backdrop(DWM_SYSTEMBACKDROP_TYPE::DWMSBT_MAINWINDOW);
+    app();
 
     airglow::Window window1{app.m_hwnd.get(), "window1"};
     window1();
+    window1.dwm_caption_color(false);
+    window1.dwm_dark_mode(true);
+    window1.dwm_system_backdrop(DWM_SYSTEMBACKDROP_TYPE::DWMSBT_MAINWINDOW);
 
     airglow::Window window2{app.m_hwnd.get(), "window2"};
     window2();
-
-    glow::console::debug(std::to_string(app.m_dpi));
-    glow::console::debug(std::to_string(app.m_scale));
-
-    // glow::console::debug(std::to_string(window1.m_dpi));
-    // glow::console::debug(std::to_string(window2.m_scale));
-
-    // MainWindow mainWindow{"Airglow"};
-    // mainWindow();
-    // mainWindow.dwm_caption_color(false);
-    // mainWindow.dwm_dark_mode(true);
-    // mainWindow.dwm_system_backdrop(DWM_SYSTEMBACKDROP_TYPE::DWMSBT_MAINWINDOW);
-    // mainWindow.m_dpi = mainWindow.dpi();
-    // mainWindow.m_scale = mainWindow.scale();
+    window2.dwm_caption_color(false);
+    window2.dwm_dark_mode(true);
+    window2.dwm_system_backdrop(DWM_SYSTEMBACKDROP_TYPE::DWMSBT_MAINWINDOW);
 
     return glow::window::message_loop();
 }
