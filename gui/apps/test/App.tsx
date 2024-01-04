f291d90254a56a3f7cd208e7ce7ef89340c42865
mthierman <mthierman@gmail.com>
2024-01-03 23:18:55 -0700

    Simplify form

--- gui/apps/test/App.tsx
+++ gui/apps/test/App.tsx
@@ -4,39 +4,28 @@ export default function App() {
     const handleMain = (e: any) => {
         e.preventDefault();
 
-        const form = document.getElementsByName("mainForm")[0] as HTMLFormElement;
+        const form = document.getElementById("mainForm") as HTMLFormElement;
 
         const rawData = new FormData(e.target);
         const data = Object.fromEntries(rawData.entries());
 
         console.log(data.mainUrl);
 
-        if (data.mainUrl != "") {
-            console.log(data.mainUrl);
-            // window.chrome.webview.postMessage({
-            //     mainCurrentPage: data.mainCurrentPage.toString().trim(),
-            // });
-        }
-
         form.reset();
     };
 
     const handleSide = (e: any) => {
-        e.preventDefault();
-
-        const form = document.getElementsByName("sideForm")[0] as HTMLFormElement;
-
-        const rawData = new FormData(e.target);
-        const data = Object.fromEntries(rawData.entries());
-
-        if (data.sideUrl.toString() != "") {
-            console.log(data.sideUrl);
-            // window.chrome.webview.postMessage({
-            //     sideCurrentPage: data.sideCurrentPage.toString().trim(),
-            // });
-        }
-
-        form.reset();
+        // e.preventDefault();
+        // const form = document.getElementsByName("sideForm")[0] as HTMLFormElement;
+        // const rawData = new FormData(e.target);
+        // const data = Object.fromEntries(rawData.entries());
+        // if (data.sideUrl.toString() != "") {
+        //     console.log(data.sideUrl);
+        //     window.chrome.webview.postMessage({
+        //         sideCurrentPage: data.sideCurrentPage.toString().trim(),
+        //     });
+        // }
+        // form.reset();
     };
 
     return (
@@ -49,14 +38,11 @@ export default function App() {
                 onSubmit={handleMain}
                 autoComplete="off"
                 spellCheck="false">
-                <label className="flex flex-grow">
-                    <input
-                        className="flex-grow rounded-lg bg-flexoki-base-paper p-2 shadow-md shadow-flexoki-base-50 outline-none dark:bg-flexoki-base-950 dark:shadow-flexoki-base-black"
-                        type="text"
-                        name="mainUrl"
-                        id="mainUrl"></input>
-                </label>
-                <input type="submit" hidden />
+                <input
+                    className="w-full rounded-lg bg-flexoki-base-paper p-2 shadow-md shadow-flexoki-base-50 outline-none dark:bg-flexoki-base-950 dark:shadow-flexoki-base-black"
+                    type="text"
+                    name="mainUrl"
+                    id="mainUrl"></input>
             </form>
 
             <form
@@ -67,14 +53,11 @@ export default function App() {
                 onSubmit={handleSide}
                 autoComplete="off"
                 spellCheck="false">
-                <label className="flex flex-grow">
-                    <input
-                        className="w-full rounded-lg bg-flexoki-base-paper p-2 shadow-md shadow-flexoki-base-50 outline-none dark:bg-flexoki-base-950 dark:shadow-flexoki-base-black"
-                        type="text"
-                        name="sideUrl"
-                        id="sideUrl"></input>
-                </label>
-                <input type="submit" hidden />
+                <input
+                    className="w-full rounded-lg bg-flexoki-base-paper p-2 shadow-md shadow-flexoki-base-50 outline-none dark:bg-flexoki-base-950 dark:shadow-flexoki-base-black"
+                    type="text"
+                    name="sideUrl"
+                    id="sideUrl"></input>
             </form>
         </div>
     );
