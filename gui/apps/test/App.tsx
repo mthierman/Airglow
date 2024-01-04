7650b2b057421ccd1c5ca5c238f22e363744be28
mthierman <mthierman@gmail.com>
2024-01-04 11:07:14 -0700

    Consolidate address bar event handlers

--- gui/apps/test/App.tsx
+++ gui/apps/test/App.tsx
@@ -14,66 +14,67 @@ export default function App() {
     const [mainUrl, setMainUrl] = useState("");
     const [sideUrl, setSideUrl] = useState("");
 
-    const handleMainChange = (event: SyntheticEvent) => {
+    const handleChange = (event: SyntheticEvent) => {
         let input = event.target as HTMLInputElement;
-        setMainUrl(input.value);
+
+        if (input.id === "mainUrl") {
+            setMainUrl(input.value);
+        }
+
+        if (input.id === "sideUrl") {
+            setSideUrl(input.value);
+        }
     };
 
-    const handleMainSubmit = (event: SyntheticEvent) => {
+    const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
-        setMainUrl(url.parseUrl(mainUrl).href);
-        window.chrome.webview.postMessage({ mainUrl: mainUrl });
+        let input = event.target as HTMLFormElement;
+
+        if (input.id === "mainForm") {
+            setMainUrl(url.parseUrl(mainUrl).href);
+            if (window.chrome.webview) window.chrome.webview.postMessage({ mainUrl: mainUrl });
+        }
+
+        if (input.id === "sideForm") {
+            setSideUrl(url.parseUrl(sideUrl).href);
+            if (window.chrome.webview) window.chrome.webview.postMessage({ sideUrl: sideUrl });
+        }
     };
 
-    const handleSideChange = (event: SyntheticEvent) => {
-        let input = event.target as HTMLInputElement;
-        setSideUrl(input.value);
-    };
-
-    const handleSideSubmit = (event: SyntheticEvent) => {
-        event.preventDefault();
-        setSideUrl(url.parseUrl(sideUrl).href);
-        window.chrome.webview.postMessage({ sideUrl: sideUrl });
-    };
-
-    const handleClipboard = async (url: string) => {
-        await navigator.clipboard.writeText(url);
-    };
+    // const handleClipboard = async (url: string) => {
+    //     await navigator.clipboard.writeText(url);
+    // };
 
     return (
         <div className="flex gap-2 bg-transparent p-2">
             <form
                 className="flex-grow"
-                name="mainForm"
                 id="mainForm"
                 method="post"
-                onSubmit={handleMainSubmit}
+                onSubmit={handleSubmit}
                 autoComplete="off"
                 spellCheck="false">
                 <input
                     className="w-full rounded-lg bg-flexoki-base-paper p-2 shadow-md shadow-flexoki-base-50 outline-none dark:bg-flexoki-base-950 dark:shadow-flexoki-base-black"
                     type="text"
-                    name="mainUrl"
                     id="mainUrl"
                     value={mainUrl}
-                    onChange={handleMainChange}></input>
+                    onChange={handleChange}></input>
             </form>
 
             <form
                 className="flex-grow"
-                name="sideForm"
                 id="sideForm"
                 method="post"
-                onSubmit={handleSideSubmit}
+                onSubmit={handleSubmit}
                 autoComplete="off"
                 spellCheck="false">
                 <input
                     className="w-full rounded-lg bg-flexoki-base-paper p-2 shadow-md shadow-flexoki-base-50 outline-none dark:bg-flexoki-base-950 dark:shadow-flexoki-base-black"
                     type="text"
-                    name="sideUrl"
                     id="sideUrl"
                     value={sideUrl}
-                    onChange={handleSideChange}></input>
+                    onChange={handleChange}></input>
             </form>
         </div>
     );
