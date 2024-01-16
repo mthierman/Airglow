f66ff7b3c45042a06f0754e3dff2b5d12da23940
mthierman <mthierman@gmail.com>
2024-01-16 13:43:54 -0700

    Add args2 for accelerator keys

--- src/browser.cxx
+++ src/browser.cxx
@@ -12,93 +12,130 @@ auto Browser::accelerator_key_pressed_handler(ICoreWebView2Controller* sender,
                                               ICoreWebView2AcceleratorKeyPressedEventArgs* args)
     -> HRESULT
 {
-    COREWEBVIEW2_KEY_EVENT_KIND kind{};
-    args->get_KeyEventKind(&kind);
+    wil::com_ptr<ICoreWebView2AcceleratorKeyPressedEventArgs2> args2;
+    args->QueryInterface(IID_PPV_ARGS(&args2));
 
-    if (kind == COREWEBVIEW2_KEY_EVENT_KIND_KEY_DOWN ||
-        kind == COREWEBVIEW2_KEY_EVENT_KIND_SYSTEM_KEY_DOWN)
+    COREWEBVIEW2_KEY_EVENT_KIND kind;
+    args2->get_KeyEventKind(&kind);
+
+    if (args2)
     {
-        UINT key{0};
-        args->get_VirtualKey(&key);
-
-        switch (key)
+        if (kind == COREWEBVIEW2_KEY_EVENT_KIND_KEY_DOWN ||
+            kind == COREWEBVIEW2_KEY_EVENT_KIND_SYSTEM_KEY_DOWN)
         {
-        case 19:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_PAUSE, 0);
-            break;
+            unsigned int key{};
+            args2->get_VirtualKey(&key);
 
-        case 76:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, 0x4C, 0);
+            switch (key)
+            {
+            case 19:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_PAUSE, 0);
+                break;
+            }
 
-            break;
+            case 76:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, 0x4C, 0);
+                break;
+            }
 
-        case 87:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, 0x57, 0);
+            case 87:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, 0x57, 0);
+                break;
+            }
 
-            break;
+            case 112:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F1, 0);
+                break;
+            }
 
-        case 112:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F1, 0);
+            case 113:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F2, 0);
+                break;
+            }
 
-            break;
+            case 114:
+            {
+                args2->put_Handled(TRUE);
+                args2->put_IsBrowserAcceleratorKeyEnabled(FALSE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F3, 0);
+                break;
+            }
 
-        case 113:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F2, 0);
+            case 115:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F4, 0);
+                break;
+            }
 
-            break;
+            case 116:
+            {
+                args2->put_Handled(FALSE);
+                args2->put_IsBrowserAcceleratorKeyEnabled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F5, 0);
+                break;
+            }
 
-        case 114:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F3, 0);
+            case 117:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F6, 0);
+                break;
+            }
 
-            break;
+            case 118:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F7, 0);
+                break;
+            }
 
-        case 115:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F4, 0);
+            case 119:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F8, 0);
+                break;
+            }
 
-            break;
+            case 120:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F9, 0);
+                break;
+            }
 
-        case 117:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F6, 0);
+            case 121:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F10, 0);
+                break;
+            }
 
-            break;
+            case 122:
+            {
+                args2->put_Handled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F11, 0);
+                break;
+            }
 
-        case 118:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F7, 0);
-
-            break;
-
-        case 119:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F8, 0);
-
-            break;
-
-        case 120:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F9, 0);
-
-            break;
-
-        case 121:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F10, 0);
-
-            break;
-
-        case 122:
-            args->put_Handled(TRUE);
-            SendMessageA(m_parent, WM_KEYDOWN, VK_F11, 0);
-
-            break;
+            case 123:
+            {
+                args2->put_Handled(FALSE);
+                args2->put_IsBrowserAcceleratorKeyEnabled(TRUE);
+                SendMessageA(m_parent, WM_KEYDOWN, VK_F12, 0);
+                break;
+            }
+            }
         }
     }
 
