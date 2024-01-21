ed788c94d147ac1888c1f03b0ec95649151db000
mthierman <mthierman@gmail.com>
2024-01-20 20:20:22 -0700

    Working window sizing for Settings

--- gui/apps/test/App.tsx
+++ gui/apps/test/App.tsx
@@ -1,50 +1,41 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useLayoutEffect } from "react";
 
 export default function App() {
     const container = useRef<HTMLDivElement | null>(null);
-    const [height, setHeight] = useState(null);
+    const [height, setHeight] = useState(0);
+    const [devicePixelRatio, setDevicePixelRatio] = useState(window.devicePixelRatio);
 
-    useEffect(() => {
-        console.log(container.current?.offsetHeight);
-        window.chrome.webview.postMessage({ offsetHeight: container.current?.offsetHeight });
-    });
-
-    useEffect(() => {
-        window.chrome.webview.postMessage({ initialized: true });
-    }, []);
-
-    // useEffect(() => {
-    //     const onMessage = (event: Event) => {
-    //         const data = (event as MessageEvent).data;
-    //         console.log(data);
-    //     };
-
-    //     window.chrome.webview.addEventListener("message", onMessage);
-
-    //     return () => {
-    //         window.chrome.webview.removeEventListener("message", onMessage);
-    //     };
-    // });
-
-    useEffect(() => {
+    useLayoutEffect(() => {
         const onResize = () => {
-            let height = container.current?.offsetHeight;
-            let width = container.current?.offsetWidth;
-            // let scale = Math.round(window.devicePixelRatio * 1e2) / 1e2;
-            let scale = Math.ceil(window.devicePixelRatio);
-            console.log(`${height} x ${scale}`);
-
-            window.chrome.webview.postMessage({ offsetHeight: height! * scale });
-            window.chrome.webview.postMessage({ offsetWidth: width! * scale });
-            window.chrome.webview.postMessage({
-                devicePixelRatio: scale,
-            });
+            setDevicePixelRatio(window.devicePixelRatio);
         };
 
         addEventListener("resize", onResize);
 
         return () => {
             removeEventListener("resize", onResize);
+        };
+    });
+
+    useEffect(() => {
+        setHeight(container.current?.offsetHeight!);
+        window.chrome.webview.postMessage({ offsetHeight: height * devicePixelRatio });
+    });
+
+    useEffect(() => {
+        window.chrome.webview.postMessage({ initialized: true });
+    }, []);
+
+    useEffect(() => {
+        const onMessage = (event: Event) => {
+            const data = (event as MessageEvent).data;
+            console.log(data);
+        };
+
+        window.chrome.webview.addEventListener("message", onMessage);
+
+        return () => {
+            window.chrome.webview.removeEventListener("message", onMessage);
         };
     });
 
@@ -59,7 +50,3 @@ export default function App() {
         </div>
     );
 }
-
-// offsetHeight: Math.round(
-//     container.current!.offsetHeight *
-//         (Math.round(window.devicePixelRatio * 1e2) / 1e2),
