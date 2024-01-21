4db85e6f44dc20ebf23b1887667986877e7c1196
mthierman <mthierman@gmail.com>
2024-01-21 01:35:35 -0700

    Checkpoint width/height

--- gui/apps/test/App.tsx
+++ gui/apps/test/App.tsx
@@ -4,7 +4,60 @@ import { getSessionStorage, getPositionStorage, getSystemColorsStorage } from "@
 import { Button } from "@components/Button";
 
 export default function App() {
-    return <>
-        <Button text="test" />
-    </>;
+    return (
+        <>
+            <Button text="test" />
+            <Button text="test" />
+            <Button text="test" />
+            <Button text="test" />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt euismod
+                tortor eget eleifend. Mauris porttitor tortor vel neque condimentum commodo. Aliquam
+                ornare posuere enim, quis laoreet magna faucibus vitae. Maecenas posuere hendrerit
+                lectus, at dignissim magna finibus eu. Sed vulputate euismod ornare. Aenean eleifend
+                sed mi quis bibendum. Etiam imperdiet elit dui, et eleifend elit auctor molestie.
+                Aenean sodales, libero quis dictum maximus, massa sem rutrum risus, non convallis
+                lorem mi sed felis. Etiam sapien arcu, finibus at ipsum at, maximus feugiat magna.
+                Pellentesque posuere purus non risus aliquam, ac imperdiet nulla porttitor. Aliquam
+                vel est quis metus viverra malesuada facilisis eu ex. Mauris aliquet hendrerit
+                neque, nec pharetra magna tristique feugiat.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt euismod
+                tortor eget eleifend. Mauris porttitor tortor vel neque condimentum commodo. Aliquam
+                ornare posuere enim, quis laoreet magna faucibus vitae. Maecenas posuere hendrerit
+                lectus, at dignissim magna finibus eu. Sed vulputate euismod ornare. Aenean eleifend
+                sed mi quis bibendum. Etiam imperdiet elit dui, et eleifend elit auctor molestie.
+                Aenean sodales, libero quis dictum maximus, massa sem rutrum risus, non convallis
+                lorem mi sed felis. Etiam sapien arcu, finibus at ipsum at, maximus feugiat magna.
+                Pellentesque posuere purus non risus aliquam, ac imperdiet nulla porttitor. Aliquam
+                vel est quis metus viverra malesuada facilisis eu ex. Mauris aliquet hendrerit
+                neque, nec pharetra magna tristique feugiat.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt euismod
+                tortor eget eleifend. Mauris porttitor tortor vel neque condimentum commodo. Aliquam
+                ornare posuere enim, quis laoreet magna faucibus vitae. Maecenas posuere hendrerit
+                lectus, at dignissim magna finibus eu. Sed vulputate euismod ornare. Aenean eleifend
+                sed mi quis bibendum. Etiam imperdiet elit dui, et eleifend elit auctor molestie.
+                Aenean sodales, libero quis dictum maximus, massa sem rutrum risus, non convallis
+                lorem mi sed felis. Etiam sapien arcu, finibus at ipsum at, maximus feugiat magna.
+                Pellentesque posuere purus non risus aliquam, ac imperdiet nulla porttitor. Aliquam
+                vel est quis metus viverra malesuada facilisis eu ex. Mauris aliquet hendrerit
+                neque, nec pharetra magna tristique feugiat.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt euismod
+                tortor eget eleifend. Mauris porttitor tortor vel neque condimentum commodo. Aliquam
+                ornare posuere enim, quis laoreet magna faucibus vitae. Maecenas posuere hendrerit
+                lectus, at dignissim magna finibus eu. Sed vulputate euismod ornare. Aenean eleifend
+                sed mi quis bibendum. Etiam imperdiet elit dui, et eleifend elit auctor molestie.
+                Aenean sodales, libero quis dictum maximus, massa sem rutrum risus, non convallis
+                lorem mi sed felis. Etiam sapien arcu, finibus at ipsum at, maximus feugiat magna.
+                Pellentesque posuere purus non risus aliquam, ac imperdiet nulla porttitor. Aliquam
+                vel est quis metus viverra malesuada facilisis eu ex. Mauris aliquet hendrerit
+                neque, nec pharetra magna tristique feugiat.
+            </p>
+        </>
+    );
 }
