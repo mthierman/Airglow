653fc33253eec6fb2da42a0bc4c6cd2693de6341
mthierman <mthierman@gmail.com>
2024-01-22 16:06:55 -0700

    Add wix extensions

--- install.ps1
+++ install.ps1
@@ -1,8 +1,4 @@
-# wix extension add WixToolset.UI.wixext -g
-wix build .\install.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
-wix build .\bundle.wxs -o build/Bundle.exe -ext WixToolset.Bal.wixext
-
-# .\VC_redist.x64.exe /install /passive /norestart
-# .\VC_redist.x64.exe /install /quiet
-# .\VC_redist.x64.exe /install
-# .\VC_redist.x64.exe /quiet
+wix extension add WixToolset.UI.wixext
+wix extension add WixToolset.Bal.wixext
+wix build .\msi.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
+wix build .\bundle.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
