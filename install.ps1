edd2e71d9ee93efbaa2423fecc6548e3d9b124ce
mthierman <mthierman@gmail.com>
2024-01-22 15:02:18 -0700

    Working minimal bundle

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 # wix extension add WixToolset.UI.wixext -g
 wix build .\install.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
-wix build .\bundle.wxs -o build/Bundle.exe
+wix build .\bundle.wxs -o build/Bundle.exe -ext WixToolset.Bal.wixext
 
 # .\VC_redist.x64.exe /install /passive /norestart
 # .\VC_redist.x64.exe /install /quiet
