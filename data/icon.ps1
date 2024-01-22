580fc3ac157370b0ac271bae334126730b5c489c
mthierman <mthierman@gmail.com>
2024-01-22 14:07:36 -0700

    Working icon replacements

--- data/icon.ps1
+++ data/icon.ps1
@@ -1,4 +1,4 @@
-function ConvertTo-WixIco
+function ConvertTo-WixIco16
 {
     param (
         $in,
@@ -7,5 +7,16 @@ function ConvertTo-WixIco
     magick convert -background none $in -define icon:auto-resize="16" $out
 }
 
-ConvertTo-WixIco WixUINewIco.svg WixUINewIco.ico
-ConvertTo-WixIco WixUIUpIco.svg WixUIUpIco.ico
+function ConvertTo-WixIco32
+{
+    param (
+        $in,
+        $out
+    )
+    magick convert -background none $in -define icon:auto-resize="32" $out
+}
+
+ConvertTo-WixIco16 ic_fluent_folder_add_16_regular.svg WixUINewIco.ico
+ConvertTo-WixIco16 ic_fluent_folder_arrow_up_16_regular.svg WixUIUpIco.ico
+ConvertTo-WixIco32 ic_fluent_error_circle_48_regular.svg WixUIExclamationIco.ico
+ConvertTo-WixIco32 ic_fluent_info_48_regular.svg WixUIInfoIco.ico
