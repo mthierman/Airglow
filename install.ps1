f0d84761c78f0b40161c2a97b205928373ffd582
mthierman <mthierman@gmail.com>
2024-01-22 16:41:33 -0700

    Update install script

--- install.ps1
+++ install.ps1
@@ -1,5 +1,7 @@
 wix extension add WixToolset.UI.wixext
 wix extension add WixToolset.Bal.wixext
 wix build .\msi.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
+wix build .\bundle-package.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
+
 # wix build .\bundle.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
-wix build .\bundle-msi.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
+# wix build .\bundle-exe-msi.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
