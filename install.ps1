0cbe2116151b5497692ddaf42c349bd83e88aa70
mthierman <mthierman@gmail.com>
2024-01-22 16:50:13 -0700

    Remove unused wxs files

--- install.ps1
+++ install.ps1
@@ -1,7 +1,4 @@
 wix extension add WixToolset.UI.wixext
 wix extension add WixToolset.Bal.wixext
 wix build .\msi.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
-wix build .\bundle-package.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
-
-# wix build .\bundle.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
-# wix build .\bundle-exe-msi.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
+wix build .\bundle.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
