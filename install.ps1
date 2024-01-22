8f056a9f95702cbcac77abdbf596981679ea8d5f
mthierman <mthierman@gmail.com>
2024-01-22 14:54:14 -0700

    Add bundle

--- install.ps1
+++ install.ps1
@@ -1,2 +1,8 @@
 # wix extension add WixToolset.UI.wixext -g
 wix build .\install.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
+wix build .\bundle.wxs -o build/Bundle.exe
+
+# .\VC_redist.x64.exe /install /passive /norestart
+# .\VC_redist.x64.exe /install /quiet
+# .\VC_redist.x64.exe /install
+# .\VC_redist.x64.exe /quiet
