216d56205a5584ac6d3f87a08bd91194ccd5eabe
mthierman <mthierman@gmail.com>
2024-01-23 00:47:15 -0700

    Add favicon to home

--- gui/apps/home/App.tsx
+++ gui/apps/home/App.tsx
@@ -1,7 +1,19 @@
+import iconRaw from "../../../data/release.svg?raw";
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        const favicon = document.createElement("link");
+        favicon.type = "image/svg+xml";
+        favicon.rel = "icon";
+        favicon.href = `data:image/svg+xml,${encodeURIComponent(iconRaw)}`;
+        document.head.appendChild(favicon);
+    }
+};
+
 export default function App() {
     return (
         <>
-            <h1 className="font-display pointer-events-none select-none text-4xl font-bold">
+            <h1 className="pointer-events-none select-none font-display text-4xl font-bold">
                 Press ⏸️ for settings
             </h1>
         </>
