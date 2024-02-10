0bebd8f4e2b110a7bc8c8b420e2ac9eaa7233b77
mthierman <mthierman@gmail.com>
2024-02-10 02:36:56 -0700

    Cleanup settings

--- gui/apps/settings/App.tsx
+++ gui/apps/settings/App.tsx
@@ -1,25 +1,12 @@
 import "@css/index.css";
 import { SyntheticEvent, useState, useRef, useEffect } from "react";
 import { parseUrl } from "@libs/url";
-import { initialize, applyColors } from "@libs/index";
+import { initialize, getState, applyColors } from "@libs/index";
 
 export default () => {
-    const form = useRef<HTMLFormElement | null>(null);
-    const inputFirst = useRef<HTMLInputElement | null>(null);
-    const inputSecond = useRef<HTMLInputElement | null>(null);
-    const [state, setState] = useState<App.State>({
-        args: ["about:blank", "about:blank"],
-        colors: {
-            accent: "",
-            accentDark1: "",
-            accentDark2: "",
-            accentDark3: "",
-            accentLight1: "",
-            accentLight2: "",
-            accentLight3: "",
-        },
-        home: ["about:blank", "about:blank"],
-    });
+    const first = useRef<HTMLInputElement | null>(null);
+    const second = useRef<HTMLInputElement | null>(null);
+    const [state, setState] = useState<App.State>(getState());
 
     useEffect(() => {
         initialize();
@@ -42,12 +29,12 @@ export default () => {
         const onEscape = (event: KeyboardEvent) => {
             if (event.key === "Escape") {
                 const obj: App.State = JSON.parse(sessionStorage.getItem("state")!);
-                if (document.activeElement === inputFirst.current) {
+                if (document.activeElement === first.current) {
                     setState((prevState) => ({
                         ...prevState,
                         home: [obj.home[0], prevState.home[1]],
                     }));
-                } else if (document.activeElement === inputSecond.current) {
+                } else if (document.activeElement === second.current) {
                     setState((prevState) => ({
                         ...prevState,
                         home: [prevState.home[0], obj.home[1]],
@@ -68,9 +55,9 @@ export default () => {
     const handleChange = (e: SyntheticEvent) => {
         const target = e.target as HTMLInputElement;
 
-        if (document.activeElement === inputFirst.current) {
+        if (document.activeElement === first.current) {
             setState((prevState) => ({ ...prevState, home: [target.value, prevState.home[1]] }));
-        } else if (document.activeElement === inputSecond.current) {
+        } else if (document.activeElement === second.current) {
             setState((prevState) => ({ ...prevState, home: [prevState.home[0], target.value] }));
         }
     };
@@ -91,13 +78,13 @@ export default () => {
 
         let post: App.State = state;
 
-        if (document.activeElement === inputFirst.current) {
-            post.home[0] = parse(0, inputFirst.current)!;
-        } else if (document.activeElement === inputSecond.current) {
-            post.home[1] = parse(1, inputSecond.current)!;
+        if (document.activeElement === first.current) {
+            post.home[0] = parse(0, first.current)!;
+        } else if (document.activeElement === second.current) {
+            post.home[1] = parse(1, second.current)!;
         } else {
-            post.home[0] = parse(0, inputFirst.current)!;
-            post.home[1] = parse(1, inputSecond.current)!;
+            post.home[0] = parse(0, first.current)!;
+            post.home[1] = parse(1, second.current)!;
         }
 
         sessionStorage.setItem("state", JSON.stringify(post));
@@ -106,9 +93,9 @@ export default () => {
 
     const handleClick = async (event: SyntheticEvent) => {
         let nativeEvent = event.nativeEvent as MouseEvent;
-        if (document.activeElement === inputFirst.current) {
+        if (document.activeElement === first.current) {
             if (nativeEvent.ctrlKey) await navigator.clipboard.writeText(state.home[0]);
-        } else if (document.activeElement === inputSecond.current) {
+        } else if (document.activeElement === second.current) {
             if (nativeEvent.ctrlKey) await navigator.clipboard.writeText(state.home[1]);
         }
     };
@@ -116,7 +103,6 @@ export default () => {
     return (
         <form
             className="settings-form"
-            ref={form}
             id="form"
             method="post"
             autoComplete="off"
@@ -127,7 +113,7 @@ export default () => {
                     <h1 className="settings-title">🌆First Home</h1>
                     <input
                         className="settings-input"
-                        ref={inputFirst}
+                        ref={first}
                         id="first"
                         type="text"
                         value={state.home[0]}
@@ -150,7 +136,7 @@ export default () => {
                     <h1 className="settings-title">🌃Second Home</h1>
                     <input
                         className="settings-input"
-                        ref={inputSecond}
+                        ref={second}
                         id="second"
                         type="text"
                         value={state.home[1]}
--- gui/libs/index.ts
+++ gui/libs/index.ts
@@ -1,6 +1,49 @@
 import blankLight from "@assets/blankLight.svg?raw";
 import blankDark from "@assets/blankDark.svg?raw";
 
+export const initialize = () => {
+    window.chrome.webview.postMessage({ initialized: true });
+};
+
+export const getState = () => {
+    const state = sessionStorage.getItem("state");
+
+    if (state) {
+        return JSON.parse(state) as App.State;
+    } else
+        return {
+            args: ["", ""],
+            colors: {
+                accent: "",
+                accentDark1: "",
+                accentDark2: "",
+                accentDark3: "",
+                accentLight1: "",
+                accentLight2: "",
+                accentLight3: "",
+            },
+            home: ["", ""],
+        } as App.State;
+};
+
+export const applyColors = (colors: App.Colors) => {
+    document.documentElement.style.setProperty("--accent", colors.accent);
+    document.documentElement.style.setProperty("--accentDark1", colors.accentDark1);
+    document.documentElement.style.setProperty("--accentDark2", colors.accentDark2);
+    document.documentElement.style.setProperty("--accentDark3", colors.accentDark3);
+    document.documentElement.style.setProperty("--accentLight1", colors.accentLight1);
+    document.documentElement.style.setProperty("--accentLight2", colors.accentLight2);
+    document.documentElement.style.setProperty("--accentLight3", colors.accentLight3);
+};
+
+export const defaultFavicon = () => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return `data:image/svg+xml,${encodeURIComponent(blankDark)}`;
+    } else {
+        return `data:image/svg+xml,${encodeURIComponent(blankLight)}`;
+    }
+};
+
 export const getSessionStorage = (key: string, defaultValue: any) => {
     const value = sessionStorage.getItem(key);
 
@@ -46,27 +89,5 @@ export const getColorStorage = () => {
         return defaultValue;
     } else {
         return value;
-    }
-};
-
-export const applyColors = (colors: App.Colors) => {
-    document.documentElement.style.setProperty("--accent", colors.accent);
-    document.documentElement.style.setProperty("--accentDark1", colors.accentDark1);
-    document.documentElement.style.setProperty("--accentDark2", colors.accentDark2);
-    document.documentElement.style.setProperty("--accentDark3", colors.accentDark3);
-    document.documentElement.style.setProperty("--accentLight1", colors.accentLight1);
-    document.documentElement.style.setProperty("--accentLight2", colors.accentLight2);
-    document.documentElement.style.setProperty("--accentLight3", colors.accentLight3);
-};
-
-export const initialize = () => {
-    window.chrome.webview.postMessage({ initialized: true });
-};
-
-export const defaultFavicon = () => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        return `data:image/svg+xml,${encodeURIComponent(blankDark)}`;
-    } else {
-        return `data:image/svg+xml,${encodeURIComponent(blankLight)}`;
     }
 };
