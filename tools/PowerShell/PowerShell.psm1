7187216ac74d7b9bf8b48aa9b1b90beb94b6d4a8
mthierman <mthierman@gmail.com>
2024-02-16 15:38:30 -0700

    pwsh: Module overhaul

--- tools/PowerShell/PowerShell.psm1
+++ tools/PowerShell/PowerShell.psm1
@@ -1,3 +1,37 @@
+function Compress-Airglow
+{
+    Push-Location
+    Set-Location "build/Release"
+    if (Test-Path "Airglow.zip") { Remove-Item "Airglow.zip" -Force }
+    7z a Airglow.zip Airglow.exe gui
+    Pop-Location
+}
+
+function Build-AirglowInstaller
+{
+    dotnet tool restore
+    dotnet wix build tools\installer\msi.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
+    dotnet wix build tools\installer\bundle.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
+}
+
+function Publish-Airglow
+{
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string]$Branch = 'main'
+    )
+
+    # $hash = Get-Content "build/notes/short_hash"
+    $version = Get-Content "build/notes/version"
+    $notes = Get-Item "build/notes/release_notes"
+    $archive = Get-Item "build/Release/Airglow.zip"
+    $installer = Get-Item "build/Airglow.exe"
+
+    gh release delete $version -y
+    gh release create $version $archive $installer --notes-file $notes -t $version
+}
+
 function Get-VCRedist
 {
     Push-Location
@@ -6,7 +40,7 @@ function Get-VCRedist
     Pop-Location
 }
 
-function Invoke-Go
+function Build-AirglowServer
 {
     go build -o "./build/Release/AirglowServer.exe" "./tools/server"
 }
@@ -16,22 +50,6 @@ function Add-WixExtensions
     dotnet tool restore
     dotnet wix extension add WixToolset.UI.wixext
     dotnet wix extension add WixToolset.Bal.wixext
-}
-
-function Invoke-Archive
-{
-    Push-Location
-    Set-Location "build/Release"
-    if (Test-Path "Airglow.zip") { Remove-Item "Airglow.zip" -Force }
-    7z a Airglow.zip Airglow.exe gui
-    Pop-Location
-}
-
-function Invoke-Wix
-{
-    dotnet tool restore
-    dotnet wix build tools\installer\msi.wxs -o build/Airglow.msi -ext WixToolset.UI.wixext
-    dotnet wix build tools\installer\bundle.wxs -o build/Airglow.exe -ext WixToolset.Bal.wixext
 }
 
 function ConvertTo-WixIco
@@ -50,47 +68,4 @@ function Convert-Icons
     ConvertTo-WixIco 16 data/ic_fluent_folder_arrow_up_16_regular.svg data/WixUIUpIco.ico
     ConvertTo-WixIco 32 data/ic_fluent_error_circle_48_regular.svg data/WixUIExclamationIco.ico
     ConvertTo-WixIco 32 data/ic_fluent_info_48_regular.svg data/WixUIInfoIco.ico
-}
-
-function Get-Version
-{
-    Get-Content "build/notes/version"
-}
-
-function Get-ShortHash
-{
-    Get-Content "build/notes/short_hash"
-}
-
-function Get-ReleaseNotes
-{
-    Get-Item "build/notes/release_notes"
-}
-
-function Get-Archive
-{
-    Get-Item "build/Release/Airglow.zip"
-}
-
-function Get-Installer
-{
-    Get-Item "build/Airglow.exe"
-}
-
-function Publish-Airglow
-{
-    [CmdletBinding()]
-    param (
-        [ValidateNotNullOrEmpty()]
-        [string]$Branch = 'main'
-    )
-
-    # $hash = Get-ShortHash
-    $version = Get-Version
-    $notes = Get-ReleaseNotes
-    $archive = Get-Archive
-    $installer = Get-Installer
-
-    gh release delete $version -y
-    gh release create $version $archive $installer --notes-file $notes -t $version
 }
