ead93f80d58c2835e230979c52d7ec92a8a249e2
mthierman <mthierman@gmail.com>
2024-02-27 20:59:34 -0700

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        plugins: {
+            "eslint-plugin-react": eslintPluginReact,
+            "eslint-plugin-react-hooks": eslintPluginReactHooks,
+        },
+        languageOptions: {
+            parserOptions: {
+                project: ["tsconfig.json", "tsconfig.node.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        files: ["**/*.js"],
+        ...tseslint.configs.disableTypeChecked,
+    },
+);
