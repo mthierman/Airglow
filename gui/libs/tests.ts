29c3fdc1d696634cdb09ba6c3fc2599f33a1c806
mthierman <mthierman@gmail.com>
2024-03-17 15:28:24 -0600

    Match C++ URL parser, seperate tests

---
+++ gui/libs/tests.ts
@@ -0,0 +1,12 @@
+const tests = {
+    error1: "https://asdasdasdasdasdfkjlwejjklsrghsjkghsjgnsegfnbsgk.com/",
+    error2: "http://www.apple",
+    http: "http://www.apple.com/",
+    https: "https://www.apple.com/",
+    www: "www.apple.com",
+    com: "google.com",
+    searchTerm: "apples",
+    spaces: "   apples asdasd",
+};
+const values = Object.values(tests);
+const searchEngine = "https://www.google.com/search?q=";
--- gui/libs/url.ts
+++ gui/libs/url.ts
@@ -1,40 +1,17 @@
-export const tests = {
-    error1: "https://asdasdasdasdasdfkjlwejjklsrghsjkghsjgnsegfnbsgk.com/",
-    error2: "http://www.apple",
-    http: "http://www.apple.com/",
-    https: "https://www.apple.com/",
-    www: "www.apple.com",
-    com: "google.com",
-    searchTerm: "apples",
-    spaces: "   apples asdasd",
-};
-export const values = Object.values(tests);
-export const searchEngine = "https://www.google.com/search?q=";
-
-const parse = (input: string): URL => {
-    let url: URL;
-
-    if (URL.canParse(input)) {
-        url = new URL(input);
-    } else {
-        url = parse(`${searchEngine}${encodeURIComponent(input)}`);
-    }
-
-    return url;
-};
-
 export default (input: string) => {
     input = input.trim();
 
     if (input.length === 0) {
-        return input;
-    }
-
-    if (input.startsWith("http://") || input.startsWith("https://")) {
-        return parse(input).href;
+        return null;
+    } else if (input.startsWith("http")) {
+        if (URL.canParse(input)) {
+            return new URL(input).href;
+        }
     } else if (input.includes(".")) {
-        return parse(`https://${input}`).href;
-    } else {
-        return parse(input).href;
-    }
+        if (URL.canParse(`https://${input}`)) {
+            return new URL(`https://${input}`).href;
+        }
+    } else if (URL.canParse("https://www.google.com/")) {
+        return new URL(`https://www.google.com/search?q=${encodeURIComponent(input)}`).href;
+    } else return null;
 };
