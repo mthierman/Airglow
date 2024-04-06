46612e2dfb736af307d50e83165500da47d1fe8c
mthierman <mthierman@gmail.com>
2024-04-05 23:15:13 -0600

    Update packages, eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,33 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import reactPluginConfigsJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
-import reactPluginConfigsRecommended from "eslint-plugin-react/configs/recommended.js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
+    reactRecommended,
+    reactJsxRuntime,
     {
         ignores: ["**/build/**"],
     },
     {
         plugins: {
-            "react": reactPlugin,
-            "react-hooks": reactHooksPlugin,
+            react,
+            // reactHooks,
         },
         rules: {
-            ...reactPluginConfigsRecommended.rules,
-            ...reactPluginConfigsJsxRuntime.rules,
-            ...reactHooksPlugin.configs.recommended.rules,
+            // ...reactHooks.configs.recommended.rules,
         },
         languageOptions: {
             globals: {
                 ...globals.browser,
+                ...globals.serviceworker,
             },
             parserOptions: {
                 project: ["tsconfig.json"],
