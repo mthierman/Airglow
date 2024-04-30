f2593161f9d420dc52326c7a87f8621350ae1893
mthierman <mthierman@gmail.com>
2024-04-29 20:36:36 -0600

    Add test script

---
+++ tools/scripts/test.ps1
@@ -0,0 +1,7 @@
+.\airglow https://www.typescriptlang.org/docs/handbook/intro.html
+# Start-Sleep -Seconds 1
+.\airglow https://www.electronjs.org/docs/latest/
+# Start-Sleep -Seconds 1
+.\airglow https://react.dev/learn
+# Start-Sleep -Seconds 1
+.\airglow https://developer.mozilla.org/en-US/docs/Web/JavaScript
