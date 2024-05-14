49698a81249924e95b3fb5719389904e1d3318f5
mthierman <mthierman@gmail.com>
2024-05-14 13:23:04 -0600

    CJS for postcss/tailwind.. *sigh*

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 
-export default {
+module.exports = {
     plugins: {
         autoprefixer: {},
         tailwindcss: {},
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-export default {
+module.exports = {
     content: ["./gui/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
         fontFamily: {
@@ -20,4 +20,4 @@ export default {
             },
         },
     },
-} satisfies Config;
+};
