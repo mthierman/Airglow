154f35d834dfc222ffcec5f07fe688be866fa7c0
mthierman <mthierman@gmail.com>
2024-07-11 14:43:55 -0600

    WM_SIZE

--- src/settings.cxx
+++ src/settings.cxx
@@ -7,15 +7,38 @@
 #include "settings.hxx"
 
 namespace airglow {
-Settings::Settings() {
-    // message(WM_GETMINMAXINFO, [this](glow::messages::wm_getminmaxinfo message) {
-    //     message.minMaxInfo().ptMinTrackSize.x = 500;
-    //     message.minMaxInfo().ptMinTrackSize.y = 500;
+Settings::Settings(glow::webview::WebViewEnvironment& webViewEnvironment)
+    : m_webViewEnvironment { webViewEnvironment } {
+    message(WM_GETMINMAXINFO, [this](glow::messages::wm_getminmaxinfo message) {
+        message.minMaxInfo().ptMinTrackSize.x = 500;
+        message.minMaxInfo().ptMinTrackSize.y = 500;
 
-    //     return 0;
-    // });
+        return 0;
+    });
+
+    message(WM_CREATE, [this](glow::messages::wm_create message) {
+        glow::window::set_backdrop(m_hwnd.get(), DWM_SYSTEMBACKDROP_TYPE::DWMSBT_TRANSIENTWINDOW);
+        glow::window::set_position(m_hwnd.get(), 0, 0, 500, 500);
+        return 0;
+    });
+
+    message(WM_CLOSE, [this](glow::messages::wm message) {
+        glow::window::hide(m_hwnd.get());
+
+        return 0;
+    });
 
     create("Settings", false);
+
+    m_webView.create(m_webViewEnvironment, m_hwnd.get(), [this]() {
+        m_webView.navigate("about:blank");
+        m_webView.put_bounds(m_hwnd.get());
+
+        message(WM_SIZE, [this](glow::messages::wm_size message) {
+            m_webView.put_bounds(m_hwnd.get());
+            return 0;
+        });
+    });
 }
 
 // auto CALLBACK Settings::EnumChildProc(::HWND hWnd, ::LPARAM lParam) -> ::BOOL {
@@ -52,34 +75,6 @@ Settings::Settings() {
 //     }
 
 //     return true;
-// }
-
-// auto Settings::on_create(::WPARAM wParam, ::LPARAM lParam) -> int {
-//     position();
-
-//     dwm_caption_color(false);
-//     dwm_system_backdrop(DWMSBT_TRANSIENTWINDOW);
-//     theme();
-
-//     ::SetWindowPos(m_hwnd.get(), nullptr, 0, 0, 500, 500, SWP_NOMOVE);
-
-//     std::function<::HRESULT()> settingsCallback { [=, this]() {
-//         m_browser->navigate(m_browser->url("settings"));
-
-//         return S_OK;
-//     } };
-
-//     m_browser = std::make_unique<Browser>(m_hwnd.get(), settingsCallback);
-//     m_browser->create_window();
-//     m_browser->reveal();
-
-//     return 0;
-// }
-
-// auto Settings::on_close(::WPARAM wParam, ::LPARAM lParam) -> int {
-//     hide();
-
-//     return 0;
 // }
 
 // auto Settings::on_key_down(::WPARAM wParam, ::LPARAM lParam) -> int {
@@ -167,13 +162,6 @@ Settings::Settings() {
 //             break;
 //         }
 //     }
-
-//     return 0;
-// }
-
-// auto Settings::on_size(::WPARAM wParam, ::LPARAM lParam) -> int {
-//     position();
-//     ::EnumChildWindows(m_hwnd.get(), EnumChildProc, reinterpret_cast<size_t>(this));
 
 //     return 0;
 // }
--- src/settings.hxx
+++ src/settings.hxx
@@ -9,24 +9,29 @@
 #include <memory>
 
 #include <glow/window.hxx>
+#include <glow/webview.hxx>
+
 #include <nlohmann/json.hpp>
 
 // #include "browser.hxx"
 // #include "global.hxx"
 // #include "state.hxx"
 
-namespace airglow{
+namespace airglow {
 using json = nlohmann::json;
 
 struct Settings final : glow::window::Window {
-    Settings();
+    Settings(glow::webview::WebViewEnvironment& webViewEnvironment);
 
     // std::unique_ptr<Browser> m_browser {};
     // Keys m_keys {};
+
+    glow::webview::WebViewEnvironment& m_webViewEnvironment;
+    glow::webview::WebView m_webView;
 
 private:
     // static auto EnumChildProc(::HWND hWnd, ::LPARAM lParam) -> ::BOOL;
 
     // NLOHMANN_DEFINE_TYPE_INTRUSIVE(Settings, m_state)
 };
-};
+}; // namespace airglow
